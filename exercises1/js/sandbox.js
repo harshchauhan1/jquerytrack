95b18429b37cce85cc62778e433146c090b5ee21
harsh chauhan <harsh@harsh.(none)>
2012-07-30 18:36:16 +0530

    jq1

---
+++ exercises1/js/sandbox.js
@@ -0,0 +1,9 @@
+console.log($( 'div.module' ));
+console.log($( '#myListItem' ));
+console.log($( '#myList #myListItem' ));
+console.log($( '#myList li:nth-child(3)' ));
+console.log($( 'li#myListItem' )); //most efficient
+console.log($( "label[for='q']"));
+console.log($( ':hidden').length);
+console.log($( 'img[alt]' ).length);
+console.log($('#fruits').find("tr:odd"));
