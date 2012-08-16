964f5f495100b776568e8978d300c87167b4268f
harsh chauhan <harsh@harsh.(none)>
2012-08-16 10:26:22 +0530

    updatedex2

--- exercise2/js/sandbox.js
+++ exercise2/js/sandbox.js
@@ -2,11 +2,11 @@ console.log( $( 'img' ));
 $( 'img' ).each( function (index) {
                  console.log(index + ': ' + $(this).attr('alt'));});
 
-console.log( $( "input[name='q']" ).parent ().attr ('class', 'myclass'));
+console.log( $( "input[name='q']" ).parent ().addClass ('myclass'));
 
-console.log($( 'li[class="current bar"]').removeAttr('class').next().attr('class', 'currrent bar'));
+console.log($( 'li[class="current bar"]').removeClass('current bar').next().addClass('currrent bar'));
 
-console.log( $ ( '#specials :submit'));
+console.log( $ ( '#specials select').closest('form').find('input[type = "submit"][value = "Go"]'));
 
-console.log($ ('#slideshow li:nth-child(1)').attr('class', 'current'));
-console.log($ ('#slideshow li:nth-child(1)').siblings().attr('class', 'disabled'));
+console.log($ ('#slideshow li:nth-child(1)').addClass('current'));
+console.log($ ('#slideshow li:nth-child(1)').siblings().addClass('disabled'));
