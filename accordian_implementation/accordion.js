0d92a2ca4aeb55ff8a08a9aa5cedda738e4e6bfb
harsh chauhan <harsh@harsh.(none)>
2012-08-21 18:00:50 +0530

    emproleupdate

--- accordian_implementation/accordion.js
+++ accordian_implementation/accordion.js
@@ -4,11 +4,13 @@ $('document').ready(function() {
     event.preventDefault();  
     if($(this).children().length == 0) { 
       var index = a_all.index($(this));
-      window.open("allornone.html?id="+index);
+      window.open("allornone.html?id="+index , '_self');
     }  
   });
   var id = window.location.search.substring(1).split("=");
   if(id[1]) {
-   console.log($($(a_all.get(id[1]))[0]).parent().css('background-color', 'white').parents().slideDown());
+   var req_element = $(a_all.get(id[1]))[0];
+   $(req_element).parent().css('background-color', 'white');
+   $(req_element).parents().show();
   }
 });
